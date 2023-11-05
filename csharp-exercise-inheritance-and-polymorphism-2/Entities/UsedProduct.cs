f2aa8e88d0c64c68428d1de4a754d850b4ddc08d
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-04 21:36:45 -0400

    feat add used product class

---
+++ csharp-exercise-inheritance-and-polymorphism-2/Entities/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_inheritance_and_polymorphism_2.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime Manufacture { get; set; }
+
+        public UsedProduct() { }
+
+        public UsedProduct(string name, double price, DateTime manufacture) : base(name, price)
+        {
+            Name = name;
+            Price = price;
+            Manufacture = manufacture;
+        }
+
+        public override string priceTag()
+        {
+            return $"{Name} (Used) R$ {Price.ToString("F2", CultureInfo.InvariantCulture)} (Manufacture date: {Manufacture.Date})";
+        }
+    }
+}
